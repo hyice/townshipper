620a96fbf97dc4ad96255bdc1480e35af1f98b24
hyice <hyice.zhang@gmail.com>
2018-01-17 22:53:11 +0800

    finish barn related logic

--- townshipper/main.py
+++ townshipper/main.py
@@ -2,37 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import window
+import time
 
-window.full_screen()
+time.sleep(3)
+# window.full_screen()
+# window.Barn.open()
+window.Barn.upgrade()
 
-# barn.open_barn()
-
-# import pyautogui
-#
-# screen_width, screen_height = pyautogui.size()
-# image_width, image_height = pyautogui.screenshot().size
-#
-# screen_scale = image_width / screen_width
-#
-# def get_wheat_field_position():
-#     pos = pyautogui.locateCenterOnScreen('resource/wheat1.png', confidence=0.8)
-#
-#     if pos is None:
-#         pos = pyautogui.locateCenterOnScreen('resource/wheat2.png', confidence=0.8)
-#
-#     return (pos[0]/screen_scale, pos[1]/screen_scale) if pos else None
-#
-#
-# wheat_position = None
-# while True:
-#     wheat_position = get_wheat_field_position()
-#
-#     if wheat_position:
-#         print('found wheat filed, ', wheat_position)
-#         pyautogui.moveTo(wheat_position[0], wheat_position[1])
-#         continue
-#
-#     print('not found wheat field')
 
 
 
--- townshipper/window.py
+++ townshipper/window.py
@@ -14,26 +14,28 @@ def _locations_by_image(image_path):
 
     return [(location[0]/screen_scale, location[1]/screen_scale) for location in locations_iter]
 
-def _location_for_image(image_path, try_times=10, confidence=0.8, **kwargs):
+
+def _location_for_image(image_path, try_times=10, confidence=0.95, **kwargs):
     left_try_times = try_times
     while left_try_times:
         location = pyautogui.locateCenterOnScreen(image_path, confidence=confidence, **kwargs)
 
         if location:
-            return (location[0]/screen_scale, location[1]/screen_scale)
+            return location[0]/screen_scale, location[1]/screen_scale
 
         left_try_times -= 1
 
     return None
 
+
 def _activate_window():
-    window_title_position = _location_for_image('resource/settings/window_title.png', confidence=0.95)
+    window_title_position = _location_for_image('resource/settings/window_title.png')
     if not window_title_position:
-        print('Failed To Activate Window!')
         return False
 
     pyautogui.click(window_title_position[0], window_title_position[1])
     return True
+
 
 def full_screen():
     if not _activate_window():
@@ -42,12 +44,59 @@ def full_screen():
     left_try_time = 3
     while left_try_time:
         pyautogui.press('esc')
-        button_position =_location_for_image('resource/settings/full_screen.png')
+        button_position = _location_for_image('resource/settings/full_screen.png', try_times=3)
 
         if button_position:
             pyautogui.click(button_position[0], button_position[1])
             pyautogui.press('esc')
-            return
+            return True
 
     return False
+
+
+class Barn:
+    RESOURCE_BASE_DIR = 'resource/barn/'
+
+    all_items = [
+        'glass', 'brick', 'slab', 'paint', 'hammer', 'nail',  # building materials
+        'wheat', 'corn', 'carrot', 'sugarcane', 'cotton',  # field plant's
+        'milk',  # animal's
+    ]
+
+    @classmethod
+    def open(cls):
+        return cls._find_and_click_image('house.png')
+
+    @classmethod
+    def upgrade(cls):
+        return cls._find_and_click_image('upgrade.png')
+
+    @classmethod
+    def detail_for_item(cls, item):
+        if item not in cls.all_items:
+            return False
+
+        return cls._find_and_click_image(item + '.png')
+
+    @classmethod
+    def increase_sell_amount(cls):
+        return cls._find_and_click_image('increase.png')
+
+    @classmethod
+    def decrease_sell_amount(cls):
+        return cls._find_and_click_image('decrease.png')
+
+    @classmethod
+    def sell(cls):
+        return cls._find_and_click_image('sell.png')
+
+    @classmethod
+    def _find_and_click_image(cls, image_name):
+        position = _location_for_image(cls.RESOURCE_BASE_DIR + image_name)
+
+        if not position:
+            return False
+
+        pyautogui.click(position[0], position[1])
+        return True
 
